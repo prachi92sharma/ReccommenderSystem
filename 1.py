83cbb5463fdfdb399f239c70e1e0c0c88ffcad4f
Vysakh P R <my_mail_is_buzy@yahoo.com.au>
2017-11-16 22:05:42 +0530

    Removed Stemmer

--- 1.py
+++ 1.py
@@ -11,16 +11,12 @@ import collections
 import numpy as np
 from collections import defaultdict
 from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
 
 months = ["january","february", "march","april","may","june","july","august","september","october","november","december"]
 wordnumbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
-stemmer = SnowballStemmer("english")
-#orig_stdout = sys.stdout
-#f = open('out.txt', 'w')
-#sys.stdout = f
 word_dict = defaultdict(int)
 stopwords_list = set(stopwords.words('english'))
+
 fp = open('preprocessed.txt','w')
 fpath = os.path.join("abstract1.txt")
 for line in open("abstract1.txt"):
@@ -30,11 +26,9 @@ for line in open("abstract1.txt"):
 	wlist = [w for w in words if w not in stopwords_list if len(w) > 2]
 	wlist = [w for w in wlist if w not in months ]
 	wlist = [w for w in wlist if w not in wordnumbers ]
-	wlist = [stemmer.stem(w) for w in wlist]
 	for word in wlist:
 		word_dict[word] += 1  
 	fp.write(' '.join(wlist)+ '\n')
 fp.close()
-#print (stopwords1)
 print (len(word_dict))
 np.save("vocab",word_dict)
