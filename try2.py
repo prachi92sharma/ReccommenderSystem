56e1f33cc31a1714ddb8404c27f006bc7cdc8696
Prachi Sharma <prachi.sharma92@gmail.com>
2017-11-16 22:52:34 +0530

    document topic list

--- try2.py
+++ try2.py
@@ -6,7 +6,7 @@ from nltk.corpus import stopwords
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 stoplist = set(stopwords.words('english'))
-fpath = os.path.join("/home/prachi.sharma92/Project/abstract1.txt")
+fpath = os.path.join("/home/prachi.sharma92/Project/preprocessed.txt")
 with open(fpath, "r") as script:
 	filelines =script.readlines()
 
@@ -17,4 +17,7 @@ dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
 #lda = LdaModel(corpus, num_topics=10)
 lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=10, alpha=0.1, eta = 0.1,update_every=1, chunksize=100, passes=1)
-lda.print_topics(10)
+#lda.print_topics(10)
+for files in filelines:
+	bow = dictionary.doc2bow(files.split())
+	print lda.get_document_topics(bow, minimum_probability=None, minimum_phi_value=None, per_word_topics=False)
