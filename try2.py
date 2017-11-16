6964d27efb7bade83f64332ac66be635d9f10ed2
Prachi Sharma <prachi.sharma92@gmail.com>
2017-11-16 23:21:17 +0530

    idk

--- try2.py
+++ try2.py
@@ -18,6 +18,7 @@ corpus = [dictionary.doc2bow(text) for text in texts]
 #lda = LdaModel(corpus, num_topics=10)
 lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=10, alpha=0.1, eta = 0.1,update_every=1, chunksize=100, passes=1)
 #lda.print_topics(10)
-for files in filelines:
-	bow = dictionary.doc2bow(files.split())
-	print lda.get_document_topics(bow, minimum_probability=None, minimum_phi_value=None, per_word_topics=False)
+#for files in filelines:
+files = filelines[1]
+bow = dictionary.doc2bow(files.split())
+print lda.get_document_topics(bow, minimum_probability=0, minimum_phi_value=None, per_word_topics=False)
